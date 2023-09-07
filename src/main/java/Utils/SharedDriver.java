bfdf887009cf523196fa9b1dfe584aa5c2c8ea55
Anastasiia <anaguschina@gmail.com>
2023-09-07 18:32:52 -0400

    new arguments to ChromeOptions

--- src/main/java/Utils/SharedDriver.java
+++ src/main/java/Utils/SharedDriver.java
@@ -27,6 +27,9 @@ public class SharedDriver {
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("--headless");
+                options.addArguments("--remote-debugging-port=9222");
+                options.addArguments("--disable-gpu");
+
 
                 WebDriverManager.chromedriver().setup();
                 webDriver = new ChromeDriver();
